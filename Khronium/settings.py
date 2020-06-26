d2f84c36b4b04f197686d192573670fade76d0c8
Kronixion <38648440+Kronixion@users.noreply.github.com>
2020-06-26 12:03:35 +0300

    changes

--- Khronium/settings.py
+++ Khronium/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '_d*28)l5zj1i5o3cz1tc^*7fa9mk53*3fsx^#p6g)dkr1=6^bv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
